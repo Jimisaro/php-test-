61261f5edf81b56af23dc24bfd2b574f8a3646e3
jimmy mulei <jimmymulei250@gmail.com>
2026-09-13 19:46:01 +0300

    my initial laracats php test

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tutorial</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo  "Recommended books to read";
+        ?>
+        </h1>
+       <?= $books = [
+        [
+            'title' => 'THE RICH DAD AND THE POOR DAD',
+            'author' => 'ROBERT T. KIYOSAKI',
+            'year' => '1997',
+            'PURCHASE URL' => 'https://www.amazon.com/Rich-Dad-Poor-Teach-Middle/dp/1612680194',
+        ],
+        [
+            'title' => 'THE 7 HABITS OF HIGHLY EFFECTIVE PEOPLE',
+            'author' => 'STEPHEN R. COVEY',
+            'year' => '1989',
+            'PURCHASE URL' => 'https://www.amazon.com/Habits-Highly-Effective-People-Powerful/dp/0743269519',
+        ],
+        [
+            'title' => 'THE POWER OF HABIT',
+            'author' => 'CHARLES DUHIGG',
+            'year' => '2012',
+            'PURCHASE URL' => 'https://www.amazon.com/Power-Habit-What-Life-Business/dp/081298160X',
+        ],
+       ]; 
+       ?>
+    <ul>
+        <?php foreach($books as $boook):?>
+            <li>
+                <a href="<?= $boook['PURCHASE URL'] ?>">
+                <?= $boook['title'] ?></a> by 
+                <?= $boook['author'] ?> (<?= $boook['year'] ?>)
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
